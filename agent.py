e237291009e4cfb601e05531835c6f51f84135c4
Nithin Krishnamurthi <nithin@nithins-mbp-2.lan>
2025-01-04 17:56:14 -0800

    Got basic agent stuff working

--- agent.py
+++ agent.py
@@ -1,5 +1,8 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
+import json
+
 load_dotenv()
 # Seed information
 SEED_INFORMATION = """
@@ -14,9 +17,24 @@ You will return a json representation of the following:
     "equation": "The mathematical notation and nothing else",
     "correct": "A correct natural language interpretation of the notation",
     "incorrect": "Three incorrect natural language interpretations of the notation",
-
 }
 """
 
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
 
-GCP_PROJECT_ID = os.getenv('OPEN_AI_KEY')
+def get_question(difficulty):
+    response = client.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": SEED_INFORMATION},
+                {"role": "user", "content": str(difficulty)}
+            ]
+        )
+    print(response)
+    decoded_data = json.loads(response.choices[0].message.content)
+    print(decoded_data)
+
+    
+        
